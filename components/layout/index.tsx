3ac7f1af4a44f90691a6573f72868cce90c2062f
Nipodemos <nipodemos13@gmail.com>
2022-03-27 23:01:05 -0300

    mais detalhes na página de Layout

--- components/layout/index.tsx
+++ components/layout/index.tsx
@@ -1,13 +1,34 @@
 import { NextPage } from "next";
 import styles from "./styles.module.scss";
+import { Menu, Pane } from "evergreen-ui";
+import Link from "next/link";
 
 export const Layout: NextPage = ({ children }) => {
   return (
-    <div className={styles.container}>
-      <nav className={styles.sidebar}>
-        <div className={styles.logo}>Logotipo</div>
-      </nav>
-      <main className={styles.mainContent}>{children}</main>
-    </div>
+    <Pane className={styles.container}>
+      <Pane is="nav" elevation={2} className={styles.sidebar}>
+        <div className={styles.logo}>
+          <Link href="/">
+            <a>Logotipo</a>
+          </Link>
+        </div>
+
+        <Menu>
+          <Menu.Item>
+            <Link href="/listar_tickets">
+              <a>Listar tickets</a>
+            </Link>
+          </Menu.Item>
+          <Menu.Item>
+            <Link href="/listar_tickets">
+              <a>Criar novo ticket</a>
+            </Link>
+          </Menu.Item>
+        </Menu>
+      </Pane>
+      <Pane is="main" className={styles.mainContent}>
+        {children}
+      </Pane>
+    </Pane>
   );
 };
